3404fc3a2ad2aa312a41093f4c58c18dbb9407ad
rosinsse <rosinsse@shernet.sheridancollege.ca>
2023-02-21 10:46:38 -0500

    I'm Done

---
+++ src/sebastian_rosinski/Sebastian.java
@@ -0,0 +1,16 @@
+package sebastian_rosinski;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author srosi
+ */
+public class Sebastian {
+    //these are some comments
+    //that are needed
+    //for the question
+}
